b9fb75ceac064e43989e87364a844a69a72f0cbd
rmnaser90 <rmnaser90@gmail.com>
2020-12-02 02:02:31 +0200

    expenses by group done

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment')
 const mongoose = require('mongoose')
+const { update } = require('../../models/expense')
 const Expense = require('../../models/expense')
 
 
-
 const getExpenses = async function () {
+ 
     const result = await Expense.find({}).sort({ date: 1 })
     return result
 }
@@ -22,6 +24,25 @@ const addExpense = async function (expense) {
     return savePromise
 }
 
+const updateGroup = async function (group1, group2) {
+    const result = Expense.findOneAndUpdate({ group: group1 }, { group: group2 })
+    return result
+}
+const findGroup = async function (group, total) {
+
+    if(total) {
+        const result = await Expense.aggregate([
+            {$match: {group}},
+            {$group: 
+                        { _id: null, 
+                        totalExpenses: { $sum: "$amount" } }}
+        ])
+        return result
+    }
+    const result = await Expense.find({group})
+    return result
+}
+
 
 
 router.get('/expenses', function (req, res) {
@@ -30,9 +51,26 @@ router.get('/expenses', function (req, res) {
 
 
 router.post('/expense', function (req, res) {
-const newExpense = req.body
-addExpense(newExpense).then((err,result) => res.send({err,result}))
+    const newExpense = req.body
+    addExpense(newExpense).then((err, result) => res.send({ err, result }))
 })
+
+
+router.put('/update', function (req, res) {
+    const group1 = req.body.group1
+    const group2 = req.body.group2
+    updateGroup(group1, group2).then(e => res.send(e.name))
+
+})
+
+
+router.get('/expenses/:group', function (req, res) {
+const group = req.params.group
+const total = req.query.total
+console.log(total);
+findGroup(group, total).then(g => res.send(g))
+})
+
 
 
 module.exports = router
